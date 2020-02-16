5150b3b94740381986990512b62f3f4e3c8839a7
jsacevedo <jordan_acevedo@yahoo.com>
2020-02-16 17:32:02 -0600

    Changing design to reflect single-page layout

--- wp-content/themes/traptreatz/front-page.php
+++ wp-content/themes/traptreatz/front-page.php
@@ -12,55 +12,42 @@
 <section class="our-story">
   <div class="our-story-container">
     <img class="our-story-title" alt="Our Story" src="<?php echo get_stylesheet_directory_uri(); ?>/images/our-story_small.gif">
-    <div class="our-story-text">
+    <article class="our-story-text">
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
       <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-    </div>
+    </article>
   </div>
 </section>
 
-<!-- Front Page Product Menu -->
-<section class="product-menu" style="background-image: url(<?php echo get_theme_file_uri('/images/brick-wall.png'); ?>);">
+<?php // Front page product menu ?>
+<section class="product-menu" style="background-image: url(<?php echo get_theme_file_uri('/images/brick-wall_small.jpg'); ?>);">
   <div class="menu-title-container">
     <h2>Menu</h2>
     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
   </div>
-  <img class="sweet-menu-title" alt="Sweet" src="<?php echo get_stylesheet_directory_uri(); ?>/images/sweet_small.gif">
-  <div class="sweet-items-list"></div>
-  <img class="savory-menu-title" alt="Sweet" src="<?php echo get_stylesheet_directory_uri(); ?>/images/savory_small.gif">
-  <div class="savory-items-list">
-    <div class="savory-menu-item">
-      <div class="item-image-container">
-      </div>
-      <div class="item-information">
-        <h3 class="item-title">Smore'z</h3>
-        <h4 class="item-price">3/$12.00</h4>
-        <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button type="button" href="#" class="add-to-cart">Add to Cart</button>
-      </div>
-    </div>
-    <div class="savory-menu-item">
-      <div class="item-image-container">
-      </div>
-      <div class="item-information">
-        <h3 class="item-title">Fruit Loops</h3>
-        <h4 class="item-price">3/$12.00</h4>
-        <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button type="button" href="#" class="add-to-cart">Add to Cart</button>
-      </div>
-    </div>
-    <div class="savory-menu-item">
-      <div class="item-image-container">
-      </div>
-      <div class="item-information">
-        <h3 class="item-title">Toffee</h3>
-        <h4 class="item-price">3/$12.00</h4>
-        <p class="item-description">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.</p>
-        <button type="button" href="#" class="add-to-cart">Add to Cart</button>
-      </div>
+  <?php // Container for sweet menu items ?>
+  <div class="sweet-items-container">
+    <img class="sweet-menu-title" alt="Sweet" src="<?php echo get_stylesheet_directory_uri(); ?>/images/sweet_small.gif">
+    <div class="sweet-items-list">
+      <?php echo do_shortcode('[products columns="1"]'); ?>
     </div>
   </div>
+
+  <?php // Container for savory menu items ?>
+  <div class="savory-items-container">
+    <img class="savory-menu-title" alt="Sweet" src="<?php echo get_stylesheet_directory_uri(); ?>/images/savory_small.gif">
+    <div class="savory-items-list">
+      
+    </div>
+  </div>
+
+  <?php // Container for merch ?>
+  <div class="merch-container">
+
+  </div>
+  
+  
 </section>
 
 <!-- Contact Section -->
--- wp-content/themes/traptreatz/functions.php
+++ wp-content/themes/traptreatz/functions.php
@@ -1,5 +1,8 @@
 <?php
 
+// Disable default WooCommerce stylesheets
+add_filter( 'woocommerce_enqueue_styles', '__return_empty_array' );
+
 function traptreatz_files() {
   wp_enqueue_script( 'traptreatz-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, microtime(), true );
   wp_enqueue_style( 'traptreatz_styles', get_stylesheet_uri(), NULL, microtime() );
@@ -32,6 +35,18 @@ add_action( 'after_setup_theme', 'traptreatz_add_woocommerce_support' );
  * */
 // Remove breadcrumb
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb', 20 );
+// Remove menu header result number
+remove_action( 'woocommerce_before_shop_loop', 'woocommerce_result_count', 20);
+// Add additional WooCommerce opening tag for background image
+add_Action( 'woocommerce_before_main_content', 'traptreatz_open_page_container' );
+function traptreatz_open_page_container() {
+  echo '<div class="menu-content-background" style="background-image: url(' . get_theme_file_uri('/images/brick-wall_small.jpg') . ');">';
+}
 // Add description to product listing
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_single_excerpt' );
+// add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_single_excerpt' );
+// Add additional WooCommerce closing tag for background image
+add_Action( 'woocommerce_after_main_content', 'traptreatz_close_page_container' );
+function traptreatz_close_page_container() {
+  echo '</div>';
+}
 ?>
